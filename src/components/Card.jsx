2fbab1188d072b12a8bfce75164685f5cc6deccf
SARA3SAEED <Swaretoo@gmail.com>
2024-06-25 13:36:45 +0300

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,16 +10,10 @@ export default function Card({char,deleteCharacter}) {
         <div key={char.id} className="card bg-base-100 w-96 m-2 p-2 shadow-xl">
 
             <img src={char.img_url} className='w-[90%] rounded '/>
-            <div className="card-body h-[400px]">
+            <div className="card-body">
                 <h2 className="card-title">{char.name}</h2>
-                <p>{char.status}</p>
-                <p>{char.species}</p>
-                <p>{char.gender}</p>
-                <p>{char.origin}</p>
-                {char.abilities && char.abilities.slice(0, 2).map((ability, index) => (
-                        <p key={index}>{ability}</p>
-                    ))}
-                <div className="card-actions justify-end">
+               
+                <div className="card-actions">
                     <button className="btn btn-primary"><Link to={`${char.id}`}>Veiw Details</Link></button>
                     <button className="btn btn-primary" onClick={() => deleteCharacter(char.id)}>Delete</button>
                 </div> 
